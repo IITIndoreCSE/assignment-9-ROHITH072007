5da7c7db15675621db838e312cc85fc74c3230cb
mems250005038 <mems250005038@iiti.ac.in>
2025-10-17 16:15:57 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+struct item{
+  int id;
+  float cost;
+};
+
+int main(){
+  item items[6];
+  
+  items[0]={101,75.50f};
+  items[1]={102,45.00f};
+  items[2]={103,120.00f};
+  items[3]={104,30.25f};
+  cout<<"items with cost greater than 50.00"<<endl;
+  for(int i=0;i<6;i++){
+    if (items[i].cost>50.00){
+      cout<<"|"<<"id:"<<items[i].id<<" | cost:"<<items[i].cost<<" |"<<endl;
+    }
+  }
+  return 0;
+}
